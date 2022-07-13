6b292bc7849ce8a2fbed0fa786c6661065b30a2b
Aniely Martins <aniellymartins@gmail.com>
2022-07-13 00:14:37 -0300

    correção autenticação do usuário

--- src/main/java/com/adoteumpet/adocao/controllers/UsuarioController.java
+++ src/main/java/com/adoteumpet/adocao/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.adoteumpet.adocao.controllers;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adoteumpet.adocao.dtos.UsuarioDTO;
+import com.adoteumpet.adocao.security.JWTUtil;
 import com.adoteumpet.adocao.services.UsuarioService;
 
 @RestController
@@ -17,10 +20,16 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService service;
+	@Autowired
+	private JWTUtil jwtUtil;
 
 	@PostMapping("/cadastrar")
-	public ResponseEntity<UsuarioDTO> criar(@RequestBody UsuarioDTO usuario) {
-		return ResponseEntity.ok().body(service.criar(usuario));
+	public ResponseEntity<UsuarioDTO> criar(@RequestBody UsuarioDTO dto,  HttpServletResponse res) {
+        UsuarioDTO usuario = service.criar(dto);
+        String token = jwtUtil.generateToken(usuario.getEmail());
+        res.addHeader("Authorization", "Bearer " + token);
+		res.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.ok().body(usuario);
 	}
 	
  
